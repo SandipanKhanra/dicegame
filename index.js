182885209fc57ca14a0c051597df7f55de61cf51
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2022-08-13 15:51:31 +0530

    Modified index.js to declare winner
    Depending on the highest value among the dices text inside h1 element is changed

--- index.js
+++ index.js
@@ -10,9 +10,19 @@ leftImg.setAttribute("src",imgSrc1);
 
 // Change the right side dice image randomly
 var randomNum2 = Math.floor(Math.random()*6)+1;
-console.log(randomNum2);
+// console.log(randomNum2);
 
 // Select the left image using query selector
 var rightImg = document.getElementsByClassName("img2")[0]; //This select
 var imgSrc2 = "images/dice"+randomNum2+".png";
 rightImg.setAttribute("src",imgSrc2);
+
+//Declare who wins depending on highest value among the dices
+var headingText = document.querySelector("h1");
+if(randomNum1>randomNum2){
+  headingText.innerHTML = "Player 1 win!";
+}else if(randomNum2>randomNum1){
+  headingText.innerHTML = "Player 2 win!";
+}else{
+  headingText.innerHTML = "Draw!!! Refresh again";
+}
