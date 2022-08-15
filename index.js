27a2ed97ced11611d97c2ef1dbe5d643b11b9db4
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2022-08-15 17:31:22 +0530

    Modified the game logic
    Added two buttons and custom CSS
    Added JS functions to control the game flow

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-// Change the left side dice image randomly
-var randomNum1 = Math.floor(Math.random()*6)+1;
-// console.log(randomNum1);
+var defaultimg = "images/dice6.png";
+var arr = [6, 6];
+// Select the left image using query selector
+var leftImg = document.getElementsByClassName("img1")[0];
 
 // Select the left image using query selector
-var leftImg = document.getElementsByClassName("img1")[0]; //This select
-var imgSrc1 = "images/dice"+randomNum1+".png";
-leftImg.setAttribute("src",imgSrc1);
-
-
-// Change the right side dice image randomly
-var randomNum2 = Math.floor(Math.random()*6)+1;
-// console.log(randomNum2);
-
-// Select the left image using query selector
-var rightImg = document.getElementsByClassName("img2")[0]; //This select
-var imgSrc2 = "images/dice"+randomNum2+".png";
-rightImg.setAttribute("src",imgSrc2);
-
-//Declare who wins depending on highest value among the dices
+var rightImg = document.getElementsByClassName("img2")[0];
 var headingText = document.querySelector("h1");
-if(randomNum1>randomNum2){
-  headingText.innerHTML = "Player 1 win!";
-}else if(randomNum2>randomNum1){
-  headingText.innerHTML = "Player 2 win!";
-}else{
-  headingText.innerHTML = "Draw!!! Refresh again";
+
+//Roll the dices when button is clicked
+var rollBtn = document.querySelector(".roll");
+rollBtn.addEventListener("click", function() {
+  generateRandNum();
+  var imgSrc1 = "images/dice" + arr[0] + ".png";
+  leftImg.setAttribute("src", imgSrc1);
+
+  var imgSrc2 = "images/dice" + arr[1] + ".png";
+  rightImg.setAttribute("src", imgSrc2);
+
+  //Declare who wins depending on highest value among the dices
+  if (arr[0] > arr[1]) {
+    headingText.innerHTML = "Player 1 win!";
+  } else if (arr[1] > arr[0]) {
+    headingText.innerHTML = "Player 2 win!";
+  } else {
+    headingText.innerHTML = "Draw!!! Roll again";
+  }
+})
+
+//Reset the game when reset button is clicked
+document.querySelector(".reset").addEventListener("click",function(){
+  reset();
+})
+
+//Generate two random number between 1 to 6
+function generateRandNum() {
+  var randomNum1 = Math.floor(Math.random() * 6) + 1;
+  arr[0] = randomNum1;
+  var randomNum2 = Math.floor(Math.random() * 6) + 1;
+  arr[1] = randomNum2;
+}
+
+function reset() {
+  headingText.innerHTML = "Let's Roll !";
+  leftImg.setAttribute("src", defaultimg);
+  rightImg.setAttribute("src", defaultimg);
 }
